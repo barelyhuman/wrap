cac2ef2d3ddee7120f93f302b00eab135ee9dfbc
Siddharth <ahoy@barelyhuman.dev>
2021-06-09 18:32:32 +0530

    chore: add husky, lint-staged

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
 #!/usr/bin/env node
 
-const rollup = require("rollup");
-const { babel } = require("@rollup/plugin-babel");
-const { resolve } = require("path");
-const { white, red, green } = require("kleur");
-const logSymbols = require("log-symbols");
-const { existsSync } = require("fs");
+const rollup = require('rollup')
+const { babel } = require('@rollup/plugin-babel')
+const { resolve } = require('path')
+const { white, red, green } = require('kleur')
+const logSymbols = require('log-symbols')
+const { existsSync } = require('fs')
 
-const important = white().bold;
-const success = green().bold;
+const important = white().bold
+const success = green().bold
 
 const inputOptions = {
   plugins: [
     babel({
-      babelHelpers: "bundled",
-    }),
-  ],
-};
-
-const outputOptions = {
-  format: "cjs",
-};
-
-function errorHandler(err) {
-  const msg = (err.message || err || "Unknown error").replace(
-    /(\r?\n)/g,
-    "$1      "
-  );
-  console.error(red().bold("wrap ") + msg);
-  process.exit(1);
+      babelHelpers: 'bundled'
+    })
+  ]
 }
 
-async function main() {
+const outputOptions = {
+  format: 'cjs'
+}
+
+function errorHandler (err) {
+  const msg = (err.message || err || 'Unknown error').replace(
+    /(\r?\n)/g,
+    '$1      '
+  )
+  console.error(red().bold('wrap ') + msg)
+  process.exit(1)
+}
+
+async function main () {
   try {
-    const pkgfile = resolve("package.json");
-    const pkg = existsSync(pkgfile) && require(pkgfile);
+    const pkgfile = resolve('package.json')
+    const pkg = existsSync(pkgfile) && require(pkgfile)
 
     if (!pkg) {
       throw new Error(
         `'package' not found, add a ${important(
-          "package.json"
+          'package.json'
         )} to your project`
-      );
+      )
     }
 
-    const entry = pkg.source;
-    const main = pkg.main;
+    const entry = pkg.source
+    const main = pkg.main
     if (!entry) {
       throw new Error(
         `'package.source' not found, add a ${important(
-          "source"
+          'source'
         )} field to your package.json`
-      );
+      )
     }
     if (!main) {
       throw new Error(
         `'package.main' not found, add a ${important(
-          "main"
+          'main'
         )} field to your package.json`
-      );
+      )
     }
 
-    const _inputOptions = { ...inputOptions, input: entry };
+    const _inputOptions = { ...inputOptions, input: entry }
 
     const _outputOptions = {
       ...outputOptions,
       file: main,
-      banner: pkg.bin ? "#!/usr/bin/env node\n" : null,
-    };
+      banner: pkg.bin ? '#!/usr/bin/env node\n' : null
+    }
 
-    const bundle = await rollup.rollup(_inputOptions);
-    await bundle.generate(_outputOptions);
-    await bundle.write(_outputOptions);
-    await bundle.close();
+    const bundle = await rollup.rollup(_inputOptions)
+    await bundle.generate(_outputOptions)
+    await bundle.write(_outputOptions)
+    await bundle.close()
     console.log(
       `${logSymbols.info} Output written to ${important(_outputOptions.file)}`
-    );
-    console.log(`${logSymbols.success} ${success("Done")}`);
+    )
+    console.log(`${logSymbols.success} ${success('Done')}`)
   } catch (err) {
-    errorHandler(err);
+    errorHandler(err)
   }
 }
 
-main();
+main()
