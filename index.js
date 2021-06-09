e22003e196a396c86f3c1435ff8754c199a309aa
Siddharth <ahoy@barelyhuman.dev>
2021-06-09 17:43:15 +0530

    fix: confirmations
    added hasbangs, fixed package.json resolution and
    added a test package.json inside the test folder

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
-const rollup = require('rollup')
-const { babel } = require('@rollup/plugin-babel')
-const { resolve, join } = require('path')
-const { white, red, green } = require('kleur')
-const logSymbols = require('log-symbols')
+#!/usr/bin/env node
 
-const important = white().bold
-const success = green().bold
+const rollup = require("rollup");
+const { babel } = require("@rollup/plugin-babel");
+const { resolve } = require("path");
+const { white, red, green } = require("kleur");
+const logSymbols = require("log-symbols");
+const { existsSync } = require("fs");
+
+const important = white().bold;
+const success = green().bold;
 
 const inputOptions = {
   plugins: [
     babel({
-      babelHelpers: 'bundled'
-    })
-  ]
-}
+      babelHelpers: "bundled",
+    }),
+  ],
+};
 
 const outputOptions = {
-  format: 'cjs'
-}
+  format: "cjs",
+};
 
-function errorHandler (err) {
-  const msg = (err.message || err || 'Unknown error').replace(
+function errorHandler(err) {
+  const msg = (err.message || err || "Unknown error").replace(
     /(\r?\n)/g,
-    '$1      '
-  )
-  console.error(red().bold('wrap ') + msg)
-  process.exit(1)
+    "$1      "
+  );
+  console.error(red().bold("wrap ") + msg);
+  process.exit(1);
 }
 
-async function main () {
+async function main() {
   try {
-    const configSource = join(__dirname, 'package.json')
-    const pkg = resolve(configSource) && require(configSource)
-    const entry = pkg.source
-    const main = pkg.main
+    const pkgfile = resolve("package.json");
+    const pkg = existsSync(pkgfile) && require(pkgfile);
+
+    if (!pkg) {
+      throw new Error(
+        `'package' not found, add a ${important(
+          "package.json"
+        )} to your project`
+      );
+    }
+
+    const entry = pkg.source;
+    const main = pkg.main;
     if (!entry) {
       throw new Error(
         `'package.source' not found, add a ${important(
-          'source'
+          "source"
         )} field to your package.json`
-      )
+      );
     }
     if (!main) {
       throw new Error(
         `'package.main' not found, add a ${important(
-          'main'
+          "main"
         )} field to your package.json`
-      )
+      );
     }
 
     const _outputOptions = {
       ...outputOptions,
-      file: main
-    }
+      file: main,
+    };
 
-    const bundle = await rollup.rollup({ ...inputOptions, input: entry })
-    await bundle.generate(_outputOptions)
-    await bundle.write(_outputOptions)
-    await bundle.close()
+    const bundle = await rollup.rollup({ ...inputOptions, input: entry });
+    await bundle.generate(_outputOptions);
+    await bundle.write(_outputOptions);
+    await bundle.close();
     console.log(
       `${logSymbols.info} Output written to ${important(_outputOptions.file)}`
-    )
-    console.log(`${logSymbols.success} ${success('Done')}`)
+    );
+    console.log(`${logSymbols.success} ${success("Done")}`);
   } catch (err) {
-    errorHandler(err)
+    errorHandler(err);
   }
 }
 
-main()
+main();
