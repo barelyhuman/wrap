c7088531bb3d1e5cab2cd29fd556079d6eee12a6
Siddharth <ahoy@barelyhuman.dev>
2021-06-17 08:28:19 +0530

    0.0.7-dev.0

--- README.md
+++ README.md
@@ -68,6 +68,7 @@ Usage
     $ wrap [options]
   Options
     -f, --fix Run the standard linter and fixer on the current directory
+    -w, --watch watch entry file and deps for changes
   Examples
     $ wrap
     ℹ Output written to dist/index.js
--- source/usage.js
+++ source/usage.js
@@ -3,6 +3,7 @@ export const usage = `
     $ wrap [options]
   Options
     -f, --fix Run the standard linter and fixer on the current directory
+    -w, --watch watch entry file and deps for changes
   Examples
     $ wrap
     ℹ Output written to dist/index.js
